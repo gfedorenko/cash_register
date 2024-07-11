6450bee1adab747bcf0c7bd9607d1f5bdc684102
Halyna Fedorenko <f.g.i.mind@gmail.com>
2024-07-11 12:18:19 +0100

    Add Product and Offer classes

---
+++ cash_register/offer.py
@@ -0,0 +1,8 @@
+class Offer:
+
+    def __init__(self, type, product, nth, discount_amount=0, discount_percent=0):
+        self.type = type
+        self.product = product
+        self.nth = nth
+        self.discount_amount = discount_amount
+        self.descount_percent = discount_percent
---
+++ data.py
@@ -0,0 +1,40 @@
+products = [
+    {
+        'code': 'GR1',
+        'name': "Green tea",
+        'price': 3.11,
+
+    },
+    {
+        'code': 'SR1',
+        'name': "Strawberries",
+        'price': 5.00,
+
+    },
+    {
+        'code': 'CF1',
+        'name': "Coffee",
+        'price': 11.23,
+
+    },
+]
+
+offers = [
+    {
+        'type': 'get_one_free',
+        'nth': 2,
+        'product': 'GR1',
+    },
+    {
+        'type': 'get_discount',
+        'product': 'SR1',
+        'nth': 3,
+        'discount_amount': 0.5,
+    },
+    {
+        'type': 'get_percent_discount',
+        'product': 'CF1',
+        'nth': 3,
+        'discount_percent': 0.66666667,
+    }
+]
